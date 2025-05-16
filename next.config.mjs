69cc7494a40172b0bb85e93c4bce91b98e2c9e64
outlander <cse1905033brur@gmail.com>
2025-05-16 15:25:38 +0600

    fix L

--- next.config.mjs
+++ next.config.mjs
@@ -4,36 +4,9 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "skillicons.dev",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "codeforces-readme-stats.vercel.app",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "codechef-readme-stats.onrender.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "leetcard.jacoblin.cool",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "atcoder-readme-stats.vercel.app",
-        pathname: "/**",
+        hostname: "**",
       },
     ],
-    dangerouslyAllowSVG: true,
   },
 };
 
